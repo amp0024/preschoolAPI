d83e1b8c53850b3f1f52c4c770097cc5dc083010
amp0024 <amp0024@gmail.com>
2016-04-25 13:49:50 -0600

    created preschoolDB and table schemas

---
+++ migrations/20160425134517_guardians.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('guardians', function(table){
+    
+    table.increments().primary();
+    table.string('firstName');
+    
+    table.string('lastName');
+    table.string('phone');
+    table.string('email');
+    table.string('username').unique();
+    table.string('password');
+  });
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('guardians');
+};
