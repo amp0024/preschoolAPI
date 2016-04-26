c690c7119fb8479bc77181b2f99cc7fadc8d8baf
Thomas Bealer <tbealer1991@gmail.com>
2016-04-26 10:14:43 -0600

    last update for creating api michaels way

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
-var routes = require("./routes/index");
+var routes = require("./src/server/routes/index");
+// var auth = require("./src/server/routes/auth");
 // var breweries = require("./routes/breweries");
 // var beers = require("./routes/beers");
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "html");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
@@ -21,9 +22,10 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
+// app.use("/auth", auth);
 // app.use("/breweries", breweries);
 // app.use("/beers", beers);
 
@@ -41,7 +43,7 @@ app.use(function(req, res, next) {
 if (app.get("env") === "development") {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render("error", {
+        res.send({
             message: err.message,
             error: err
         });
@@ -52,7 +54,7 @@ if (app.get("env") === "development") {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render("error", {
+    res.send({
         message: err.message,
         error: {}
     });
