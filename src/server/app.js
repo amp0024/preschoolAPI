e1f822517044f45c3510be273faa308c15322669
Thomas Bealer <tbealer1991@gmail.com>
2016-04-26 13:29:00 -0600

    db up and running on heroku

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dotenv = require('dotenv').config();
 
 
 // *** routes *** //
