7204be83255160f4e6497f240173b002aab921f0
Michael Herman <hermanmu@gmail.com>
2016-04-27 11:43:04 -0600

    added jwt auth

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -3,87 +3,50 @@ var router = express.Router();
 
 var knex = require('../db/knex');
 // var queries = require('../db/queries');
-var passport = require('../lib/auth');
 var helpers = require('../lib/helpers');
 
-// login School admins -> teacher = false -> admin = true
-router.get('/loginAdmin', helpers.loginRedirect, function(req, res, next) {
-  req.session.returnTo = req.header('referer');
-  res.render('./auth/login', { messages: req.flash('messages') });
-});
 
-router.post('/loginAdmin', function(req, res, next) {
-  passport.authenticate('local', function(err, user) {
-    if (err) {
-      req.flash('messages', {
-        status: 'danger',
-        value: 'Incorrect email and/or password.'
-      });
-      return res.redirect('/auth/login');
-    } else {
-      req.logIn(user, function(err) {
-        if (err) {
-          return next(err);
-        } else {
-          req.flash('messages', {
-            status: 'success',
-            value: 'Welcome!'
-          });
-          res.redirect(req.session.returnTo || '/');
-          delete req.session.returnTo;
-        }
-      });
-    }
-  })(req, res, next);
-});
+// router.post('/login', function(req, res, next) {
+//   passport.authenticate('local', function(err, user) {
+//     if (err) {
+//       req.flash('messages', {
+//         status: 'danger',
+//         value: 'Incorrect email and/or password.'
+//       });
+//       return res.redirect('/auth/login');
+//     } else {
+//       req.logIn(user, function(err) {
+//         if (err) {
+//           return next(err);
+//         } else {
+//           req.flash('messages', {
+//             status: 'success',
+//             value: 'Welcome!'
+//           });
+//           res.redirect(req.session.returnTo || '/');
+//           delete req.session.returnTo;
+//         }
+//       });
+//     }
+//   })(req, res, next);
+// });
 
-// login parent
-
-router.get('/login', helpers.loginRedirect, function(req, res, next) {
-  req.session.returnTo = req.header('referer');
-  res.render('./auth/login', { messages: req.flash('messages') });
-});
-
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user) {
-    if (err) {
-      req.flash('messages', {
-        status: 'danger',
-        value: 'Incorrect email and/or password.'
-      });
-      return res.redirect('/auth/login');
-    } else {
-      req.logIn(user, function(err) {
-        if (err) {
-          return next(err);
-        } else {
-          req.flash('messages', {
-            status: 'success',
-            value: 'Welcome!'
-          });
-          res.redirect(req.session.returnTo || '/');
-          delete req.session.returnTo;
-        }
-      });
-    }
-  })(req, res, next);
-});
-
-router.get('/register', helpers.loginRedirect, function(req, res, next) {
-  res.render('./auth/register', { messages: req.flash('messages') });
-});
-
-router.post('/register', function(req, res, next) {
+router.post('/register', helepers.ensureAdmin, function(req, res, next) {
   var email = req.body.email;
   var password = req.body.password;
+  var role = req.body.role;
+  var phone = req.body.phone;
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var schoolid = req.body.schoolID;
+  var image = req.body.image;
   knex('users').where('email', email)
-    .then(function(data){
+    .then(function(data) {
       if(data.length) {
-          req.flash('messages', {
-            status: 'danger',
-            value: 'Email already exists.!'
-          });
-          return res.redirect('/auth/register');
+        return res.status(409).json({
+          status: 'fail',
+          message: 'Email already exists'
+        });
       } else {
         var hashedPassword = helpers.hashing(password);
         knex('users')
@@ -91,18 +54,21 @@ router.post('/register', function(req, res, next) {
         .insert({
           email: email,
           password: hashedPassword,
-          admin: false
+          role: role,
+          phone: phone,
+          firstName: firstName,
+          lastName: lastName,
+          schoolid: schoolid,
+          image: image
         })
         .then(function(data) {
-          req.logIn(data[0], function(err) {
-            if (err) {
-              return next(err);
-            } else {
-              req.flash('messages', {
-                status: 'success',
-                value: 'Welcome!'
-              });
-              return res.redirect('/');
+          var token = generateToken(user);
+          delete user.password;
+          res.status(200).json({
+            status: 'success',
+            data: {
+              token: token,
+              user: user
             }
           });
         })
@@ -116,31 +82,23 @@ router.post('/register', function(req, res, next) {
     });
 });
 
-router.get('/logout', helpers.ensureAuthenticated, function(req, res, next) {
-  req.logout();
-  req.flash('messages', {
-    status: 'success',
-    value: 'Goodbye!'
-  });
-  res.redirect('/');
-});
 
-if (process.env.NODE_ENV === 'development') {
-  router.get('/make-admin', helpers.ensureAuthenticated,
-    function(req, res, next) {
-    queries.makeAdmin(req.user.id)
-    .then(function(books){
-      req.flash('messages', {
-        status: 'success',
-        value: 'You are now an admin!'
-      });
-      res.redirect('/');
-    })
-    .catch(function(err){
-      return next(err);
-    });
-  });
-}
+// if (process.env.NODE_ENV === 'development') {
+//   router.get('/make-admin', helpers.ensureAuthenticated,
+//     function(req, res, next) {
+//     queries.makeAdmin(req.user.id)
+//     .then(function(books){
+//       req.flash('messages', {
+//         status: 'success',
+//         value: 'You are now an admin!'
+//       });
+//       res.redirect('/');
+//     })
+//     .catch(function(err){
+//       return next(err);
+//     });
+//   });
+// }
 
 
 module.exports = router;
